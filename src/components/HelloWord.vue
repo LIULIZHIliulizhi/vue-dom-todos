960ab97c2ae54111521a5e185148dbefdc7d5324
liulizhi <2967120911@qq.com>
2018-11-11 12:49:58 +0800

    vue-base

---
+++ src/components/HelloWord.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+  data(){
+    return {
+      msg:'你好，世界'
+    }
+  }
+  }
+</script>
+
+<style scoped>
+h2{
+  color:palevioletred;
+  font-size:24px;
+}
+</style>
